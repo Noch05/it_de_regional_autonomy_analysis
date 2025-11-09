3e4f957c111efeb0cdf70751964e0f9f24f5bb35
Noah Ochital <noahochital@icloud.com>
2025-11-09 20:08:09 +0100

    Cleaning up all tables

--- code/02-model.r
+++ code/02-model.r
@@ -3,6 +3,7 @@ library(here)
 library(tidymodels)
 library(tidyverse)
 library(texreg)
+library(stargazer)
 
 df <- read_rds(here("data/it_de_regional_data_cleaned.rds"))
 # GDP Models
@@ -41,7 +42,6 @@ gdp_tex <- texreg(
   gdp_mod,
   file = here("results/gdp.tex"),
   stars = numeric(0),
-  custom.model.names = c("Simple", "Region FE", "Region and Year FE"),
   custom.coef.map = list(
     "federalTRUE" = "Federal",
     "years_since" = "Years Since 1977",
@@ -51,11 +51,23 @@ gdp_tex <- texreg(
   ),
   digits = 5,
   booktabs = TRUE,
-  dcolumn = TRUE,
   caption = "GDP Growth Rate Models",
   label = "",
   caption.above = TRUE,
-  sideways = TRUE
+  sideways = TRUE,
+  use.packages = FALSE,
+  include.ci = FALSE,
+  include.se = TRUE,
+  custom.gof.names = c(
+    NA,
+    NA,
+    "N"
+  ),
+  custom.gof.rows = list(
+    "Regional Fixed Effects" = c("", ".", "."),
+    "Year Fixed Effects" = c("", "", ".")
+  ),
+  include.rmse = FALSE,
 )
 
 texreg(
@@ -66,12 +78,6 @@ texreg(
     "GDP Per Capita Growth Rates" = 1:3,
     "GDP Per Capita" = 4
   ),
-  custom.model.names = c(
-    "Simple",
-    "Region FE",
-    "Region FE and Year FE",
-    "Region FE and Year FE"
-  ),
   custom.coef.map = list(
     "federalTRUE" = "Federal",
     "years_since" = "Years Since 1977",
@@ -81,9 +87,47 @@ texreg(
   ),
   digits = 5,
   booktabs = TRUE,
-  dcolumn = TRUE,
+  use.packages = FALSE,
   caption = "GDP Per Capita Models",
   label = "",
   caption.above = TRUE,
-  sideways = TRUE
+  sideways = TRUE,
+  include.ci = FALSE,
+  include.se = TRUE,
+  custom.gof.names = c(
+    NA,
+    NA,
+    "N"
+  ),
+  custom.gof.rows = list(
+    "Regional Fixed Effects" = c("", ".", ".", "."),
+    "Year Fixed Effects" = c("", "", ".", ".")
+  ),
+  include.rmse = FALSE,
+)
+
+all_percent_changes <- c(
+  map_dbl(gdp_mod, \(x) exp(coef(x)[2]) - 1),
+  map_dbl(gdp_pc_mod, \(x) exp(coef(x)[2]) - 1)
+)[-7] *
+  100
+
+percent_df <- data.frame(
+  Model = c(
+    "GDP 1",
+    'GDP 2',
+    "GDP 3",
+    "Per Capita 1",
+    "Per Capita 2",
+    "Per Capita 3"
+  ),
+  `Percent_Change` = all_percent_changes
+)
+
+stargazer(
+  percent_df,
+  summary = FALSE,
+  header = FALSE,
+  out = "results/percent_change.tex",
+  float = TRUE
 )
