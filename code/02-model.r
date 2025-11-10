01bd3a79187e0ca24d3555dd2a3b74ba759b0348
Noah Ochital <noahochital@icloud.com>
2025-11-10 17:58:18 +0100

    modelings using `{plm}` package

--- code/02-model.r
+++ code/02-model.r
@@ -1,16 +1,66 @@
 library(here)
 library(plm)
 library(tidyverse)
-library(texreg)
-library(xtable)
 
 options(scipen = 999)
 df <- read_rds(here("data/it_de_regional_data_cleaned.rds"))
 
 #-------------------------------------------
+# Region is entity, Year is time
+pdf <- pdata.frame(df, index = c("regions", "year")) |>
+  select(
+    year,
+    years_since,
+    gov_type,
+    federal,
+    pop,
+    gdp,
+    gdp_pc,
+    occ_agr,
+    occ_ind,
+    occ_ser
+  )
 
-pdf <- pdata.frame(df, index = "regions")
-
+# effect = "twoways" is both, effect = "individual" is region,
+# effect = "time" is year
+# model = "random" is a Random Effects, model = "within" is a fixed effects.
+# The index variables in the effects do not need to be in the formula
 # GDP Models
+gdp_models <- list(
+  simple = plm(I(log(gdp)) ~ gov_type, model = "pooling", data = pdf),
+  lin = plm(
+    I(log(gdp)) ~ gov_type + years_since,
+    effect = "individual",
+    model = "random",
+    data = pdf
+  ),
+  full = plm(
+    I(log(gdp)) ~ gov_type + year + occ_ind + occ_agr + occ_ser,
+    effect = "individual",
+    model = "random",
+    data = pdf
+  )
+)
 
 # GDP Per Capita Models
+gdp_pc_models <- list(
+  simple = plm(I(log(gdp_pc)) ~ gov_type, model = "pooling", data = pdf),
+  lin = plm(
+    I(log(gdp_pc)) ~ gov_type + years_since,
+    effect = "individual",
+    model = "random",
+    data = pdf
+  ),
+  full = plm(
+    I(log(gdp_pc)) ~ gov_type + year + occ_ind + occ_agr + occ_ser,
+    effect = "individual",
+    model = "random",
+    data = pdf
+  ),
+  nominal = plm(
+    gdp_pc ~ gov_type + year + occ_ind + occ_agr + occ_ser,
+    effect = "individual",
+    model = "random",
+    data = pdf
+  )
+)
